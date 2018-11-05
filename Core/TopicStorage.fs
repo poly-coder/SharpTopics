556bd9e338482737eba1688c2c36ea391ec2f344
Iskander <iskander.sierra@gmail.com>
2018-11-05 11:52:30 +0100

    Initial commit

---
+++ Core/TopicStorage.fs
@@ -0,0 +1,18 @@
+﻿namespace SharpTopics.Core
+
+type TopicKey = string
+type TopicSequence = uint64
+type TopicTimestamp = uint64
+
+type TopicState = {
+    messageCount: uint64
+    nextSequence: TopicSequence
+    lastTimestamp: TopicTimestamp option
+}
+
+type ITopicStorage =
+    abstract getState: unit -> Async<TopicState>
+
+type ITopicStorageFactory =
+    abstract create: TopicKey -> ITopicStorage
+
